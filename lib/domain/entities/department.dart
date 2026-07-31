f3866f52f401c5b72573ae9379c82e2d8dce69be
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-07-31 10:07:52 +0700

    feat(domain): connect departments to organization hierarchy

--- lib/domain/entities/department.dart
+++ lib/domain/entities/department.dart
@@ -4,13 +4,39 @@ class Department {
     required this.id,
     required this.code,
     required this.name,
+    this.organizationId = '',
+    this.branchId = '',
+    this.parentDepartmentId = '',
     this.active = true,
   });
 
   final String id;
   final String code;
   final String name;
+  final String organizationId;
+  final String branchId;
+  final String parentDepartmentId;
   final bool active;
+
+  Department copyWith({
+    String? id,
+    String? code,
+    String? name,
+    String? organizationId,
+    String? branchId,
+    String? parentDepartmentId,
+    bool? active,
+  }) {
+    return Department(
+      id: id ?? this.id,
+      code: code ?? this.code,
+      name: name ?? this.name,
+      organizationId: organizationId ?? this.organizationId,
+      branchId: branchId ?? this.branchId,
+      parentDepartmentId: parentDepartmentId ?? this.parentDepartmentId,
+      active: active ?? this.active,
+    );
+  }
 
   @override
   bool operator ==(Object other) =>
@@ -19,8 +45,19 @@ class Department {
           id == other.id &&
           code == other.code &&
           name == other.name &&
+          organizationId == other.organizationId &&
+          branchId == other.branchId &&
+          parentDepartmentId == other.parentDepartmentId &&
           active == other.active;
 
   @override
-  int get hashCode => Object.hash(id, code, name, active);
+  int get hashCode => Object.hash(
+        id,
+        code,
+        name,
+        organizationId,
+        branchId,
+        parentDepartmentId,
+        active,
+      );
 }
