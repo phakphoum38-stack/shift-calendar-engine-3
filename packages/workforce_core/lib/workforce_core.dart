6e9a84ff35c7eb36f1cdac9b2d94ac78efbd1a4a
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-01 03:17:28 +0700

    feat: export roster constraint engine

--- packages/workforce_core/lib/workforce_core.dart
+++ packages/workforce_core/lib/workforce_core.dart
@@ -6,3 +6,8 @@ export 'src/organization/branch.dart';
 export 'src/organization/department.dart';
 export 'src/organization/team.dart';
 export 'src/organization/organization_repository.dart';
+
+export 'src/roster/shift_assignment.dart';
+export 'src/roster/roster_constraint.dart';
+export 'src/roster/roster_validation.dart';
+export 'src/roster/constraint_engine.dart';
