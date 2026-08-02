399c037d3dbba4495143d6a9c7820c9d3ee5c4af
phakphoum38-stack <229357841+phakphoum38-stack@users.noreply.github.com>
2026-08-02 19:34:24 +0700

    feat: export AI optimization APIs

--- packages/workforce_core/lib/workforce_core.dart
+++ packages/workforce_core/lib/workforce_core.dart
@@ -27,7 +27,9 @@ export 'src/scheduler/scheduler_engine.dart';
 export 'src/scheduler/greedy_scheduler.dart';
 
 export 'src/ai_scheduler/ai_schedule_explanation.dart';
+export 'src/ai_scheduler/ai_schedule_optimizer.dart';
 export 'src/ai_scheduler/ai_schedule_proposal.dart';
+export 'src/ai_scheduler/ai_schedule_simulation.dart';
 export 'src/ai_scheduler/ai_scheduler_assistant.dart';
 export 'src/ai_scheduler/ai_scheduler_rule_engine.dart';
 export 'src/ai_scheduler/deterministic_ai_scheduler.dart';
